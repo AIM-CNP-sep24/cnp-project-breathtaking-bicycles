4e3ece834ede197af5ee2e1c96f980e251cdf20c
TiesMorshuis <Tiesmorshuis@gmail.com>
2025-04-17 12:10:06 +0200

    Frontend & backend opzet toegevoegd

---
+++ backend/breathtakingbicycles/src/main/java/com/breathtakingbicycles/BreathtakingbicyclesApplication.java
@@ -0,0 +1,13 @@
+package com.breathtakingbicycles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreathtakingbicyclesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreathtakingbicyclesApplication.class, args);
+	}
+
+}
---
+++ backend/breathtakingbicycles/src/test/java/com/breathtakingbicycles/BreathtakingbicyclesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.breathtakingbicycles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreathtakingbicyclesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
