31cef985c0c277e5aa3409e8176b0a4ea4c0fae1
SashavanRee <150018652+SashavanRee@users.noreply.github.com>
2025-04-23 16:12:11 +0200

    Update terugknop element na feedback

--- frontend/Breathtaking Bicycles/src/terugknop.jsx
+++ frontend/Breathtaking Bicycles/src/terugknop.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { ArrowLeftIcon } from '@heroicons/react/24/solid';
 
 const TerugKnop = ({ onClick }) => {
     return (
@@ -7,13 +6,10 @@ const TerugKnop = ({ onClick }) => {
             onClick={onClick}
             className="px-4 rounded-xl pb-2 w-60 h-20"
             style={{ backgroundColor: '#DDA853'}}
-
         >
             <p className="text-white text-5xl font-bold text-center">Terug</p>
-            
         </button>
     );
 };
-
 
 export default TerugKnop;
