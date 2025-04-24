02905cb33adc071e55a3e75119b9ab8a9d2fc12e
Yunus Kaya <yunusemrekaya@protonmail.com>
2025-04-24 09:35:41 +0200

    Commiting last changes

--- frontend/Breathtaking Bicycles/src/Translate.jsx
+++ frontend/Breathtaking Bicycles/src/Translate.jsx
@@ -1,17 +1,37 @@
 // Dit is een temporary component voor google translate code die later op de goede plek word gezet.
 
 function Translate() {
-    const textToTranslate = "Hello world"
-    const {Translate} = require('@google-cloud/translate').v2;
+    const apiKey = "AIzaSyDDnFCP5O3dI8dr0cTe5yQ9nGEcNFl9JSY";
+    const textToTranslate = "Hello world!"
 
-  return (
-    <>
-        <p class="mt-4">Text to translate:</p>
-        <p>{textToTranslate}</p>
-        <p class="mt-4">Detected language:</p>
-        <p class="mt-4">Translated text:</p>
-    </>
-  )
+    async function detectLanguage(detectionText) {
+        const url = "https://translation.googleapis.com/language/translate/v2/detect?q=" 
+        + detectionText +
+        "&key=" + apiKey;
+
+        try {
+            const response = await fetch(url, {method: "post"});
+            if (!response.ok) {
+                throw new Error(`Response status: ${response.status}`);
+            }
+        
+            const json = await response.json();
+            console.log(json);
+        } catch (error) {
+            console.error(error.message);
+        }
+    }      
+
+    detectLanguage(textToTranslate);
+
+    return (
+        <>
+            <p class="mt-4">Text to translate:</p>
+            <p>{textToTranslate}</p>
+            <p class="mt-4">Detected language:</p>
+            <p class="mt-4">Translated text:</p>
+        </>
+    )
 }
 
 export default Translate
