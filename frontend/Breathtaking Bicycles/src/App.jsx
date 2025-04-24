2d816abac82571836466705b571c45d11ed729e5
TiesMorshuis <Tiesmorshuis@gmail.com>
2025-04-24 10:49:18 +0200

    Benodigdheid element + meerdere op de pagina gemaakt

--- frontend/Breathtaking Bicycles/src/App.jsx
+++ frontend/Breathtaking Bicycles/src/App.jsx
@@ -1,13 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import BenodigdhedenPagina from './BenodigdhedenPagina.jsx'
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <BenodigdhedenPagina />
     </>
   )
 }
