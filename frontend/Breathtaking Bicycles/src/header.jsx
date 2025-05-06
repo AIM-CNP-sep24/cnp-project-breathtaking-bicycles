6b75d414a21e99964d2a8c90687ba7ec71506198
SashavanRee <150018652+SashavanRee@users.noreply.github.com>
2025-05-06 10:09:01 +0200

    Header onClick fix

--- frontend/Breathtaking Bicycles/src/header.jsx
+++ frontend/Breathtaking Bicycles/src/header.jsx
@@ -2,16 +2,18 @@ import React from 'react';
 import { Bars3Icon, ExclamationTriangleIcon } from '@heroicons/react/24/solid'
 
 
-const Header = ({ onClick }) => {
+const Header = ({ onClickBurger, onClickNoodknop }) => {
     return (
        <header className="bg-[#27548A] p-3">
         <div className="container mx-auto flex justify-between items-center">
-            <button className="text-white">
+            <button 
+            onClick={onClickBurger}
+            className="text-white">
                 <Bars3Icon className="h-20 w-17"/>
             </button>
 
             <button
-                onClick={onClick}
+                onClick={onClickNoodknop}
                 className="bg-red-600 text-white flex items-center gap-2 font-bold py-2 px-4">
                 <ExclamationTriangleIcon className="h-14 w-14 text-white"/>
             </button>
