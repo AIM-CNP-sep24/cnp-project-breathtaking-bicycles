9d74914753a56533ff11cd35ba8376c207d27081
Jonah610 <150021554+Jonah610@users.noreply.github.com>
2025-05-08 14:59:57 +0200

    styling aangepast meer volgens wireframe

--- frontend/Breathtaking Bicycles/src/App.jsx
+++ frontend/Breathtaking Bicycles/src/App.jsx
@@ -1,14 +1,16 @@
 import './App.css'
 import BenodigdhedenPagina from './BenodigdhedenPagina.jsx'
 import {Routes, Route} from 'react-router'
+import Textfield from './translate-components/textfield.jsx'
 
 function App() {
   
   return (
      <>
-      <Routes>          
+      {/* <Routes>          
         <Route path={"benodigdheden/:parentId"} element={<BenodigdhedenPagina />} />
-      </Routes>
+      </Routes> */}
+      <Textfield/>
     </>
   )
 }
