e253abcc9de49126a1276928a41a33e82f1d01ee
Jonah610 <150021554+Jonah610@users.noreply.github.com>
2025-05-09 09:24:58 +0200

    app.jsx terug gezet was tijdelijk aangepast

--- frontend/Breathtaking Bicycles/src/App.jsx
+++ frontend/Breathtaking Bicycles/src/App.jsx
@@ -1,16 +1,14 @@
 import './App.css'
 import BenodigdhedenPagina from './BenodigdhedenPagina.jsx'
 import {Routes, Route} from 'react-router'
-import Textfield from './translate-components/textfield.jsx'
 
 function App() {
   
   return (
      <>
-      {/* <Routes>          
+      <Routes>          
         <Route path={"benodigdheden/:parentId"} element={<BenodigdhedenPagina />} />
-      </Routes> */}
-      <Textfield/>
+      </Routes>
     </>
   )
 }
