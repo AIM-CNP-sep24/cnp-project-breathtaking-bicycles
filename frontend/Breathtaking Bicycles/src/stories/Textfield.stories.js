14ba0ba93ad2fa750f6e3eb4111b528b7050266f
Yunus Kaya <yunusemrekaya@protonmail.com>
2025-05-09 09:59:08 +0200

    Merged main and fixed visual tests

--- frontend/Breathtaking Bicycles/src/stories/Textfield.stories.js
+++ frontend/Breathtaking Bicycles/src/stories/Textfield.stories.js
@@ -1,15 +1,8 @@
-import { fn } from '@storybook/test';
-
 import Textfield from '../translate-components/textfield';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 export default {
   title: 'Translate/Textfield',
   component: Textfield,
-  parameters: {
-    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
-    layout: 'centered',
-  }
 };
 
 export const Visual =  {};
