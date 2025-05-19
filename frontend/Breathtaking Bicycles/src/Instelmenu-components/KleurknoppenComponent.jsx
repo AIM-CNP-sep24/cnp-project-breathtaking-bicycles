52eefdbb0170298c6652d67271485ec2b5119ca5
SashavanRee <150018652+SashavanRee@users.noreply.github.com>
2025-05-19 15:48:33 +0200

    Code op basis van comments aangepast.

--- frontend/Breathtaking Bicycles/src/Instelmenu-components/KleurknoppenComponent.jsx
+++ frontend/Breathtaking Bicycles/src/Instelmenu-components/KleurknoppenComponent.jsx
@@ -1,18 +1,23 @@
+
+
 function KleurknoppenComponent({ kleur, setKleur, palet }) {
+  const isSelected = kleur === palet.naam;
   return (
-    <button
-      style={{ 
-        border: kleur === palet.naam ? '4px solid #27548A' : '2px solid #ccc', // randkleur wordt aangepast op basis van de geselecteerde kleur
-        borderRadius: 12, 
-        padding: 12, 
-        boxShadow: '0 8px 12px 0 rgba(0,0,0,0.18)', // schaduw onder de knop
-        transition: 'box-shadow 0.2s, border 0.2s',
-      }}
-      onClick={setKleur}
-      className="flex flex-col items-center focus:outline-none hover:shadow-lg hover:border-[#DDA853]"
-    >
-      <img src={palet.img} alt={palet.naam + " palet"} style={{ width: 200, height: 60, marginBottom: 8 }} />
-      <div className="text-lg font-semibold text-[#222] drop-shadow-sm">{palet.naam}</div>
+    <button>
+      <div
+        className={`flex flex-col items-center justify-center bg-white hover:bg-[#DDA853] border rounded-lg shadow-md p-4 cursor-pointer focus:outline-none shadow-lg 
+        ${isSelected ? 'border-4 border-[#7a827c]' : 'border border-gray-300'}`}
+        onClick={setKleur}
+      >
+        <img
+          className={`w-50 h-20 rounded-lg ${isSelected ? 'border-4 border-[#7a827c]' : 'border border-gray-300'}`}
+          src={palet.img}
+          alt={palet.naam + " palet"} />
+        {isSelected ? 'Geselecteerd' : 'Selecteer'}
+        <p className="text-lg font-semibold text-[#222] drop-shadow-sm">
+          {palet.naam}
+        </p>
+      </div>
     </button>
   );
 }
--- frontend/Breathtaking Bicycles/src/KleurenpaletKeuzeComponent.jsx
+++ frontend/Breathtaking Bicycles/src/KleurenpaletKeuzeComponent.jsx
@@ -2,8 +2,8 @@ import { useState } from 'react';
 import KleurknoppenComponent from './Instelmenu-components/KleurknoppenComponent';
 
 const kleurPaletten = [
-  { naam: "standaard", img: "/images/kleurenpalet-standaard.png"},
-  { naam: "kleurenblinden", img: "/images/kleurenpalet-kleurenblindheid.png"}
+  { id: "1", color1: "#F5EEDC", color1Shadow: "#e0d9c8", color2: "#27548A", color2Shadow: "", color3: "#183B4E", color3Shadow: "", color4: "#DDA853", color4Shadow: "#BA8C43", img: "/images/kleurenpalet-standaard.png"},
+  { id: "2", color1: "", color1Shadow: "", color2: "", color2Shadow: "", color3: "", color3Shadow: "", color4: "", color4Shadow: "", img: "/images/kleurenpalet-kleurenblindheid.png"}
 ];
 
 // TODOO Hele applicatie verandert van kleurenpalet
@@ -11,7 +11,7 @@ function KleurenpaletKeuzeComponent() {
   const [geselecteerdPalet, setGeselecteerdPalet] = useState(''); //state om het geselecteerde kleurenpalet op te slaan
 
   return (
-    <div className="container p-20 h-100 justify-start">
+    <div className="container p-15 h-100 justify-start">
       <div className="flex flex-col h-[100%] rounded-lg bg-[#ffffff]">
         <h1 className="text-4xl font-bold mb-4 text-center">Kleurenpalet Keuze</h1>
         <div className="flex justify-between w-full px-20 gap-8">
