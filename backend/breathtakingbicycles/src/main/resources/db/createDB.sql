5b7d9dcfdc82a36abf07cd4c95b4e810b6935156
Jonah610 <150021554+Jonah610@users.noreply.github.com>
2025-05-19 14:31:02 +0200

    requested changes

--- backend/breathtakingbicycles/src/main/resources/db/createDB.sql
+++ backend/breathtakingbicycles/src/main/resources/db/createDB.sql
@@ -1,24 +1,22 @@
---DROP TABLE dbo.vertaling;
---DROP TABLE dbo.benodigdheid_vertaling;
---DROP TABLE dbo.benodigdheid;
---DROP TABLE dbo.taal;
---DROP TABLE dbo.zin;
+
+CREATE DATABASE breathtakingbicycles
+use breathtakingbicycles
 
 CREATE TABLE taal (
-    id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    naam varchar(100) NOT NULL,
-    code varchar(10) NOT NULL,
+                      id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                      naam varchar(100) NOT NULL,
+                      code varchar(10) NOT NULL,
 );
 
 CREATE TABLE zin (
-    id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                     id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
 );
 
 CREATE TABLE vertaling (
-    id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    taal_id bigint NOT NULL,
-    zin_id bigint NOT NULL,
-    tekst varchar(max) NOT NULL,
+                           id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                           taal_id bigint NOT NULL,
+                           zin_id bigint NOT NULL,
+                           tekst varchar(max) NOT NULL,
 
 	CONSTRAINT FK_vertaling_taal
         FOREIGN KEY (taal_id)
@@ -32,22 +30,24 @@ CREATE TABLE vertaling (
 );
 
 CREATE TABLE benodigdheid (
-    id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    parent_id bigint NULL,
-    laag int NOT NULL CHECK (laag BETWEEN 0 AND 2),
-    imgsrc varchar(255) NOT NULL,
+                              id bigint NOT NULL PRIMARY KEY,
+                              parent_id bigint NULL,
+                              laag int NULL,
+                              imgsrc varchar(255) NOT NULL,
+                              rangnr int NULL,
 
-    CONSTRAINT FK_benodigdheid_parent
-      FOREIGN KEY (parent_id)
-          REFERENCES benodigdheid(id)
-          ON DELETE NO ACTION
+                              CONSTRAINT CHK_rangnr_zesoflager
+                                  CHECK (rangnr <= 6),
+
+                              CONSTRAINT CHK_laag_tussen0en2
+                                  CHECK (laag >= 0 AND laag <= 2)
 );
 
 CREATE TABLE benodigdheid_vertaling (
-    id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
-    taal_id bigint NOT NULL,
-    benodigdheid_id bigint NOT NULL,
-    tekst varchar(max) NOT NULL,
+                                        id bigint NOT NULL IDENTITY(1,1) PRIMARY KEY,
+                                        taal_id bigint NOT NULL,
+                                        benodigdheid_id bigint NOT NULL,
+                                        tekst varchar(max) NOT NULL,
 
 	CONSTRAINT FK_benodigdheid_vertaling_taal
 		FOREIGN KEY (taal_id)
