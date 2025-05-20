c83436fbe5d7a9373fb5e33c5116044b50efa148
Jonah610 <150021554+Jonah610@users.noreply.github.com>
2025-05-20 11:19:40 +0200

    basis error handeling toegevoegd

--- frontend/Breathtaking-Bicycles/src/Login.jsx
+++ frontend/Breathtaking-Bicycles/src/Login.jsx
@@ -1,26 +1,27 @@
 import { useState } from 'react';
 
 function Login() {
-    // State hooks to store the username and password
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
 
-    // Handle form submission
     const handleLogin = () => {
-        console.log('Username:', username);
-        console.log('Password:', password);
+        //TODO login handeling
+
+        //Error handeling
+        setError('Gebruikersnaam of wachtwoord incorrect!');
     };
 
     return (
         <div className="flex flex-col justify-center px-4 bg-white h-200">
             <p className="mx-auto w-full mb-8 text-4xl font-semibold text-center">Inloggen</p>
             <div className="w-full mx-auto flex justify-center p-8">
-                <div className="w-96 p-8 m-auto rounded-[10px] bg-[#F5EEDC] shadow-lg">
+                <div className="w-200 p-8 mx-auto rounded-[10px] bg-[#F5EEDC]">
                     <input
                         type="text"
                         maxLength="50"
                         placeholder="Gebruikersnaam"
-                        className="h-12 pl-4 w-full mb-4 rounded-[10px] bg-[#FFFFFF]"
+                        className="h-12 w-150 pl-4 m-15 rounded-[10px] flex justify-center bg-[#FFFFFF]"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
@@ -28,18 +29,22 @@ function Login() {
                         type="password"
                         maxLength="50"
                         placeholder="Wachtwoord"
-                        className="h-12 pl-4 w-full mb-4 rounded-[10px] bg-[#FFFFFF]"
+                        className="h-12 w-150 pl-4 mt-5 m-15 rounded-[10px] flex justify-center bg-[#FFFFFF]"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
             </div>
+            <p className="w-full h-20 flex justify-center " >{error}</p>
             <button 
-                className="w-50 h-10 mx-auto flex justify-center py-2 bg-[#D18A1D] text-white rounded-[10px] text-lg font-semibold hover:bg-[#B57215] focus:outline-none focus:ring-2 focus:ring-[#D18A1D]"
+                className="cursor-pointer w-50 h-15 mx-auto flex justify-center py-4 bg-[#D18A1D] text-black rounded-[10px] text-2xl hover:bg-[#B57215] focus:outline-none focus:ring-2 focus:ring-[#D18A1D]"
                 onClick={handleLogin}
             >
-                Login
+                <p className='w-full h-full text-2xl'>
+                    Login
+                </p>
             </button>
+            
         </div>
     );
 }
