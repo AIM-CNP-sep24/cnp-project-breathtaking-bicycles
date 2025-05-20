2a1ccff6c49e2a2de6d9878ba632ef585029870b
Jonah610 <150021554+Jonah610@users.noreply.github.com>
2025-05-20 12:12:32 +0200

    kleur aanpassen

--- frontend/Breathtaking-Bicycles/src/Login.jsx
+++ frontend/Breathtaking-Bicycles/src/Login.jsx
@@ -13,15 +13,15 @@ function Login() {
     };
 
     return (
-        <div className="flex flex-col justify-center px-4 bg-white h-200">
-            <p className="mx-auto w-full mb-8 text-4xl font-semibold text-center">Inloggen</p>
+        <div className="flex flex-col justify-center px-4 h-200">
+            <p className="mx-auto w-full mb-8 text-4xl text-center">Inloggen</p>
             <div className="w-full mx-auto flex justify-center p-8">
                 <div className="w-200 p-8 mx-auto rounded-[10px] bg-[#F5EEDC]">
                     <input
                         type="text"
                         maxLength="50"
                         placeholder="Gebruikersnaam"
-                        className="h-12 w-150 pl-4 m-15 rounded-[10px] flex justify-center bg-[#FFFFFF]"
+                        className="h-12 w-150 pl-4 m-15 rounded-[10px] flex justify-center bg-white"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
                     />
@@ -29,7 +29,7 @@ function Login() {
                         type="password"
                         maxLength="50"
                         placeholder="Wachtwoord"
-                        className="h-12 w-150 pl-4 mt-5 m-15 rounded-[10px] flex justify-center bg-[#FFFFFF]"
+                        className="h-12 w-150 pl-4 mt-5 m-15 rounded-[10px] flex justify-center bg-white"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
@@ -37,7 +37,7 @@ function Login() {
             </div>
             <p className="w-full h-20 flex justify-center " >{error}</p>
             <button 
-                className="cursor-pointer w-50 h-15 mx-auto flex justify-center py-4 bg-[#D18A1D] text-black rounded-[10px] text-2xl hover:bg-[#B57215] focus:outline-none focus:ring-2 focus:ring-[#D18A1D]"
+                className="cursor-pointer w-50 h-15 mx-auto flex justify-center py-4 bg-[#DDA853] rounded-[10px] text-2xl focus:outline-none focus:ring-2 focus:ring-[#D18A1D]"
                 onClick={handleLogin}
             >
                 <p className='w-full h-full text-2xl'>
@@ -48,5 +48,4 @@ function Login() {
         </div>
     );
 }
-
 export default Login;
