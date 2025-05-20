c1d13c4150f6ab00a1f685807c37b995bdfb8658
Jonah610 <150021554+Jonah610@users.noreply.github.com>
2025-05-20 13:57:15 +0200

    form tag

--- frontend/Breathtaking-Bicycles/src/Login.jsx
+++ frontend/Breathtaking-Bicycles/src/Login.jsx
@@ -16,7 +16,13 @@ function Login() {
         <div className="flex flex-col justify-center px-4 h-200">
             <p className="mx-auto w-full mb-8 text-4xl text-center">Inloggen</p>
             <div className="w-full mx-auto flex justify-center p-8">
-                <div className="w-200 p-8 mx-auto rounded-[10px] bg-[#F5EEDC]">
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault();
+                        handleLogin();
+                    }}
+                    className="w-200 p-8 mx-auto rounded-[10px] bg-[#F5EEDC]"
+                >
                     <input
                         type="text"
                         maxLength="50"
@@ -33,18 +39,17 @@ function Login() {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                     />
-                </div>
+                    <p className="w-full h-20 flex justify-center">{error}</p>
+                    <button
+                        type="submit"
+                        className="cursor-pointer w-50 h-15 mx-auto flex justify-center py-4 bg-[#DDA853] rounded-[10px] text-2xl focus:outline-none focus:ring-2 focus:ring-[#D18A1D]"
+                    >
+                        <p className="w-full h-full text-2xl">
+                            Login
+                        </p>
+                    </button>
+                </form>
             </div>
-            <p className="w-full h-20 flex justify-center " >{error}</p>
-            <button 
-                className="cursor-pointer w-50 h-15 mx-auto flex justify-center py-4 bg-[#DDA853] rounded-[10px] text-2xl focus:outline-none focus:ring-2 focus:ring-[#D18A1D]"
-                onClick={handleLogin}
-            >
-                <p className='w-full h-full text-2xl'>
-                    Login
-                </p>
-            </button>
-            
         </div>
     );
 }
