76fd1605de8a31060bcc5c4625ef4a86eaf95454
SashavanRee <150018652+SashavanRee@users.noreply.github.com>
2025-05-22 10:17:02 +0200

    MeldingComponent + handleOpslaan opzet

--- frontend/Breathtaking Bicycles/src/InstelContainer.jsx
+++ frontend/Breathtaking Bicycles/src/InstelContainer.jsx
@@ -1,13 +1,30 @@
-import react from "react";
+import { useState } from 'react';
 import TaalInstelmenuComponent from "./TaalInstelmenuComponent.jsx";
 import KleurenpaletKeuzeComponent from "./KleurenpaletKeuzeComponent.jsx";
+import MeldingComponent from "./Instelmenu-components/MeldingComponent.jsx";
 
- function InstelContainer() {
+function InstelContainer() {
+    const [melding, setMelding] = useState(null); // state om de melding op te slaan
+
+    const handleOpslaan = async () => {
+        try {
+            //bij succes
+            setMelding({ type: 'succes', tekst: 'Instellingen zijn succesvol opgeslagen!' });
+        } catch (error) {
+            //bij error
+            setMelding({ type: 'error', tekst: 'Opslaan mislukt. Probeer het opnieuw.' });
+        }
+        setTimeout(() => setMelding(null), 3000); // melding verdwijnt na 5 seconden
+    };
+
+
     return (
         <div className="container p-10 h-200">
             <div className="flex flex-col items-center h-[100%] rounded-lg bg-[#F5EEDC]">
+                <MeldingComponent melding={melding} />
                 <TaalInstelmenuComponent />
                 <KleurenpaletKeuzeComponent />
+                <button onClick={handleOpslaan}>Opslaan</button>
             </div>
         </div>
     );
