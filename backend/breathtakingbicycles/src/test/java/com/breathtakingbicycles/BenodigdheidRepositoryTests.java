91c32dd8c0dab3b346c0e2baa12e039580504c66
TiesMorshuis <Tiesmorshuis@gmail.com>
2025-06-05 14:43:03 +0200

    BenodigdheidRepositoryTest pipeline fix

--- backend/breathtakingbicycles/src/test/java/com/breathtakingbicycles/BenodigdheidRepositoryTests.java
+++ backend/breathtakingbicycles/src/test/java/com/breathtakingbicycles/BenodigdheidRepositoryTests.java
@@ -10,8 +10,10 @@ import com.breathtakingbicycles.repository.BenodigdheidTitelRowMapper;
 import com.breathtakingbicycles.repository.BenodigdheidsChildRowMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -24,7 +26,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.*;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class BenodigdheidRepositoryTests {
 
     @InjectMocks
