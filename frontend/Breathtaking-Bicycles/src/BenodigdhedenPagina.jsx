160d53abb4df70578a3d6e1198b90916b0868c11
TiesMorshuis <Tiesmorshuis@gmail.com>
2025-06-05 11:19:08 +0200

    2de linting error fix

--- frontend/Breathtaking-Bicycles/src/BenodigdhedenPagina.jsx
+++ frontend/Breathtaking-Bicycles/src/BenodigdhedenPagina.jsx
@@ -82,10 +82,8 @@ function BenodigdhedenPagina({uiSettings, selectedLanguageZorgverlener, selected
         })()}
             
             <div className="grid-rows-3 text-center mx-5">
-                {benodigdhedenArray.map((benodigdheid, i) => {
-                    return <>
-                        <CategorieElement key={benodigdheid.id} benodigdheid={benodigdheid} id={benodigdheid.id.toString()} uiSettings={uiSettings} />
-                    </>
+                {benodigdhedenArray.map((benodigdheid) => {
+                    return (<CategorieElement key={benodigdheid.id} benodigdheid={benodigdheid} id={benodigdheid.id.toString()} uiSettings={uiSettings} />)
                 })}
             </div>
         </>
